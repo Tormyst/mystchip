abdae1d65b16c465f5d55aa5a03acbe806fb2ec8
Raphael Bronfman-Nadas <raphael@dal.ca>
2018-08-03 12:52:35 -0300

    Adding basic loop and adding a game from file
    Stubs for graphics and input setup
    Better debug output of memory
    Loading a game into 0x200

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,61 @@
+use std::fs::File;
+use std::io;
+use std::process::Command;
+
 mod cpu;
 mod mem;
 use cpu::Cpu;
 use mem::Mem;
+// use std::fmt::Debug;
 
-use std::io;
+//use std::io;
+
+#[derive(Debug)]
+struct Chip8 {
+    cpu: Cpu,
+    mem: Mem,
+    // Screen
+}
+
+impl Chip8 {
+    pub fn new() -> Chip8 {
+        Chip8 {
+            cpu: Cpu::new(),
+            mem: Mem::test_graphic()
+        }
+    }
+
+    pub fn load(&mut self, gamefile: &str) -> Result<usize, io::Error> {
+        let f = File::open(gamefile)?;
+        self.mem.load(f)
+    }
+    pub fn cycle(&mut self) {}
+
+    pub fn display(&self) {
+        print!("{}", String::from_utf8_lossy(&
+        Command::new("clear").output()
+            .expect("Failed to clear screen").stdout
+        ));
+        print!("{}", self.mem);
+    }
+}
 
 fn main(){
-    println!("{:?}", Cpu::new());
-    println!("{:?}", Mem::new());
-    println!("{}", Mem::new());
-    println!("{}", Mem::test_graphic());
+    run();
 }
+
+fn run(){
+    setupGraphics();
+    setupInput();
+
+    let mut chip8 = Chip8::new();
+    chip8.load("pong.ch8");
+    println!("{:?}", chip8);
+    loop {
+        chip8.cycle();
+        chip8.display();
+    }
+}
+
+fn setupGraphics(){}
+fn setupInput(){}
--- src/mem.rs
+++ src/mem.rs
@@ -1,4 +1,7 @@
 use std::fmt;
+use std::io;
+use std::io::Read;
+use std::fs::File;
 
 pub struct Mem {
     mem: [u8; 4096],
@@ -26,6 +29,10 @@ impl Mem {
         Mem { mem: [0; 4096], gfx }
     }
 
+    pub fn load(&mut self, mut prog: File) -> Result<usize, io::Error> {
+        prog.read(&mut self.mem[0x200..])
+    }
+
     fn gfx_read(&self, row: usize, col: usize) -> bool {
         self.gfx[gfx_offset(row, col)]
     }
@@ -33,7 +40,18 @@ impl Mem {
 
 impl fmt::Debug for Mem {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Mem")
+        writeln!(f, "Mem");
+        let mut index = 0u16;
+        writeln!(f, "      0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F");
+        for val in self.mem.chunks(16) {
+            write!(f, "{:04X}: ", index);
+            for num in val {
+                write!(f, "{:02X} ", num);
+            }
+            index += 0x10;
+            writeln!(f, "");
+        }
+        write!(f, "")
     }
 }
 

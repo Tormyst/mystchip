4d5e51d8c074b0e07313a1d315a21d8ee602d142
Raphael Bronfman-Nadas <raphael@dal.ca>
2018-08-05 13:36:35 -0300

    Added more instructions
    Added registers dt and st also, as some implemented instructions require
    them

--- src/cpu.rs
+++ src/cpu.rs
@@ -11,7 +11,9 @@ pub struct Cpu {
     reg:[u8; 16],
     i: u16,
     pc: u16,
-    stack: Vec<u16>
+    stack: Vec<u16>,
+    dt: u8,
+    st: u8,
 }
 
 fn high_nibble(num: u8) -> u8 {(num  >> 4) & 0x0F}
@@ -24,11 +26,7 @@ fn fuze(high: u8, low: u8) -> u16 {
 
 impl Cpu {
     pub fn new() -> Cpu {
-        Cpu { i: 0, pc: 0x200, reg: [0; 16], stack: Vec::new() }
-    }
-
-    pub fn new_reg(reg: [u8; 16]) -> Cpu {
-        Cpu { i: 0, pc: 0x200, reg, stack: Vec::new()}
+        Cpu { i: 0, pc: 0x200, reg: [0; 16], stack: Vec::new(), dt: 0, st: 0 }
     }
 
     fn increment_pc(&mut self) {
@@ -46,15 +44,57 @@ impl Cpu {
         println!("Instruction fetched: {:02X} {:02X}", inst_byte_upper, inst_byte_lower);
         //decode
         match  high_nibble(inst_byte_upper) {
+            0x0 => {
+                match inst_byte_upper {
+                    0x00 => {
+                        match inst_byte_lower {
+                            0xEE => { // 00EE Return from function call
+                                self.pc = self.stack.pop().unwrap();
+                                Ok(ScreenUpdate::No) 
+                            }
+                            _ => {Err(format!("Instruction {:02X} {:02X}", inst_byte_upper, inst_byte_lower))}
+                        }
+                    }
+                    _ => {Err(format!("Instruction {:02X} {:02X}", inst_byte_upper, inst_byte_lower))}
+                }
+            }
+            0x1 => {  // 1NNN Jump to NNN
+                self.pc = fuze(low_nibble(inst_byte_upper), inst_byte_lower); 
+                Ok(ScreenUpdate::No) 
+            }
             0x2 => {  // 2NNN Call function at NNN
                 self.stack.push(self.pc);
                 self.pc = fuze(low_nibble(inst_byte_upper), inst_byte_lower); 
+                Ok(ScreenUpdate::No) 
+            }
+            0x3 => {  // 3XNN Branches over next instruction if VX = NN
+                if self.reg[low_nibble(inst_byte_upper) as usize] == inst_byte_lower {
+                    self.increment_pc();
+                }
+                Ok(ScreenUpdate::No) 
+            }
+            0x4 => {  // 4XNN Branches over next instruction if VX != NN
+                if self.reg[low_nibble(inst_byte_upper) as usize] != inst_byte_lower {
+                    self.increment_pc();
+                }
+                Ok(ScreenUpdate::No) 
+            }
+            0x5 => {  // 3XY0 Branches over next instruction if VX = VY
+                if self.reg[low_nibble(inst_byte_upper) as usize] == 
+                    self.reg[high_nibble(inst_byte_lower) as usize] {
+                    self.increment_pc();
+                }
                 Ok(ScreenUpdate::No) 
             }
             0x6 => {  // 6XNN Sets VX to NN 
                 self.reg[low_nibble(inst_byte_upper) as usize] = inst_byte_lower; 
                 Ok(ScreenUpdate::No) 
             } 
+            0x7 => {  // 7XNN Adds NN to VX
+                let x = low_nibble(inst_byte_upper) as usize;
+                self.reg[x] = self.reg[x].overflowing_add(inst_byte_lower).0;
+                Ok(ScreenUpdate::No) 
+            }
             0xA => {  // ANNN Sets I to NNN
                 self.i = fuze(low_nibble(inst_byte_upper), inst_byte_lower); 
                 Ok(ScreenUpdate::No) 
@@ -62,7 +102,7 @@ impl Cpu {
             0xD => {  // DXYN Draws 
                 let x = self.reg[low_nibble(inst_byte_upper) as usize];
                 let y = self.reg[high_nibble(inst_byte_lower) as usize];
-                let n = low_nibble(inst_byte_upper);
+                let n = low_nibble(inst_byte_lower);
                 let mut ret = false;
                 for iter in 0..n {
                     let sprite = mem.read(self.i + (iter as u16));
@@ -75,6 +115,41 @@ impl Cpu {
                     self.reg[0xF] = 0u8;
                 }
                 Ok(ScreenUpdate::Yes)
+            }
+            0xF => {
+                match  inst_byte_lower {
+                    0x07 => { // FX07 set VX to DT
+                        self.reg[low_nibble(inst_byte_upper) as usize] = self.dt;
+                        Ok(ScreenUpdate::No)
+                    }
+                    0x15 => { // FX15 set DT to VX
+                        self.dt = self.reg[low_nibble(inst_byte_upper) as usize];
+                        Ok(ScreenUpdate::No)
+                    }
+                    0x18 => { // FX15 set ST to VX
+                        self.st = self.reg[low_nibble(inst_byte_upper) as usize];
+                        Ok(ScreenUpdate::No)
+                    }
+                    0x29 => { // FX29 I is set to the font location indicated by VX
+                        self.i = (self.reg[low_nibble(inst_byte_upper) as usize] * 5)
+                            as u16;
+                        Ok(ScreenUpdate::No)
+                    }
+                    0x33 => { // FX33 BCD puts VX decimal values into i, i+1 and i+2
+                        let bin = self.reg[low_nibble(inst_byte_upper) as usize];
+                        mem.write(self.i, bin / 100);
+                        mem.write(self.i + 1u16, (bin % 100) / 10);
+                        mem.write(self.i + 2u16, bin % 10);
+                        Ok(ScreenUpdate::No)
+                    }
+                    0x65 => {
+                       for index in 0..(1 + low_nibble(inst_byte_upper)) as usize {
+                           self.reg[index] = mem.read(self.i + index as u16);
+                       }
+                       Ok(ScreenUpdate::No)
+                    }
+                    _ => {Err(format!("Instruction {:02X} {:02X}", inst_byte_upper, inst_byte_lower))}
+                }
             }
             _ => {Err(format!("Instruction {:02X} {:02X}", inst_byte_upper, inst_byte_lower))}
         }
